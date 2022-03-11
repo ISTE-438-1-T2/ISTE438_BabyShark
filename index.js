92a4462e4260e914ad8252807ee93f358a3730d3
Aidan LeMay <aidanpathways12@gmail.com>
2022-03-10 19:52:09 -0500

    Removing index.js from root directory, appending startup scripts

--- index.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 80;
-app.get('/',(req, res) => res.send('Hello World'));
-app.listen(PORT, () => console.log(`Server listening in port ${PORT}`));
