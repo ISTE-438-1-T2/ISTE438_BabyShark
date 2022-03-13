cdeeb63dba8cc3e0eea1e5582b617e84f6d41dec
mastermajorman <devinmochrie@gmail.com>
2022-03-12 19:06:58 -0500

    Refactored a bit
    - Main search works
    - Modal works
    
    (project still needs: styling, dedicated image DB, and geolocation)

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const BusinessModel = require('./models/Business');
+const BusinessLocationModel = require('./models/BusinessLocation');
 
 const app = express();
 
@@ -9,7 +10,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/sharkdb',
         useNewUrlParser: true, useUnifiedTopology: true
     })
     .then(() => console.log("connection successful"))
-    .catch((err) => console.log(err));
+    .catch((error) => console.log(error));
 
 //Get the default connection
 var db = mongoose.connection;
@@ -17,11 +18,33 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.get('/episode', (req, res) => {
-    // searching for letter 'A' returns multiple
-    BusinessModel.find( { title: new RegExp(req.query.title, "i") }, 'title description season episode shark1 shark2 shark3 shark4 shark5 coords website askedFor exchangeForStake location category deal entrepreneurs image', function (err, business) {
-      if (err) return handleError(err);
-      res.send(business);
-    })
+    if(req.query.title) {
+        //business title search
+        BusinessModel.find( { title: new RegExp(req.query.title, "i") }, 'deal description episode category entrepreneurs location website askedFor exchangeForStake valuation season shark1 shark2 shark3 shark4 shark5 title coords image', function (error, results) {
+            if (error) return (error);
+            res.send(results);
+        });
+    } else if(req.query.coords) {
+        let lat = req.query.coords.split(" ").join("").split(",")[0];
+        let long = req.query.coords.split(" ").join("").split(",")[1];
+        console.log(lat + "_" + long);
+        //coords search (finds businesses within 25 miles of search
+        BusinessLocationModel.find({
+            location: {
+             $near: {
+              $maxDistance: 40000,
+              $geometry: {
+               type: "Point",
+               coordinates: [long, lat]
+              }
+             }
+            }
+           }).find((error, results) => {
+                console.log(error);
+                if (error) return (error);
+                res.send(results);
+           });
+    }
 });
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
--- src/server/models/Business.js
+++ src/server/models/Business.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const BusinessSchema = new mongoose.Schema({
     deal: {
-        type: Boolean,
+        type: String,
         required: true
     },
     description: {
