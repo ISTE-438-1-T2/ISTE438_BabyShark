cccdd4f80c2e90c338cda9dbb5cdad5c3b29c6b4
Aidan LeMay <aidanpathways12@gmail.com>
2022-03-16 15:11:14 -0400

    Updating CORS Fix

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,9 @@ module.exports = {
       '/api': 'http://localhost:8080'
     },
     headers: {
-      "Access-Control-Allow-Origin": "*"
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
     }
   },
   plugins: [
