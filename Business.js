a17a825d516ace8ec6f3eaf3e9698db863ca83f4
dbailey4018 <91158482+dbailey4018@users.noreply.github.com>
2022-03-25 11:46:17 -0400

    Add files via upload

---
+++ Business.js
@@ -0,0 +1,114 @@
+const mongoose = require('mongoose');
+
+const BusinessSchema = new mongoose.Schema({
+    _id: {
+        type: mongoose.ObjectId,
+        required: true
+    },
+    deal: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    episode: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    entrepreneurs: {
+        type: String,
+        required: true
+    },
+	//Blown up during geo query work, use script if need to add locations.
+    //location: {
+
+    //},
+    website: {
+        type: String,
+        required: true
+    },
+    askedFor: {
+        type: Number,
+        required: true
+    },
+    exchangeForStake: {
+        type: Number,
+        required: true
+    },
+    valuation: {
+        type: Number,
+        required: true
+    },
+    season: {
+        type: Number,
+        required: true
+    },
+    shark1: {
+        type: String,
+        required: true
+    },
+    shark2: {
+        type: String,
+        required: true
+    },
+    shark3: {
+        type: String,
+        required: true
+    },
+    shark4: {
+        type: String,
+        required: true
+    },
+    shark5: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    'episode-season': {
+        type: String,
+        required: true
+    },
+    'Multiple Entreprenuers': {
+        type: Boolean,
+        required: true
+    },
+	//Old coords, in case we need it for some reason
+    //coords: {
+        //type: Object,
+        //required: true
+    //},
+	coords: {
+		type: { type: String, default: "Point" },
+		coordinates: [],
+	},
+    image: {
+        type: String,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: false
+    }
+},
+
+
+{ 
+    collection : 'sharkepisodes' 
+}
+);
+
+
+//moving index to here instead of new schema
+BusinessSchema.index({ coords: "2dsphere"});
+
+const Business = mongoose.model('BusinessData', BusinessSchema);
+module.exports = Business;
---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const BusinessModel = require('./models/Business');
+const BusinessLocationModel = require('./models/BusinessLocation');
+var cors = require('cors');
+
+const app = express();
+const corsOptions = {
+    origin: "http://localhost:3000"
+};
+
+app.use(cors(corsOptions));
+
+mongoose.connect('mongodb://127.0.0.1:27017/sharkdb', 
+    {
+        useNewUrlParser: true, useUnifiedTopology: true
+    })
+    .then(() => console.log("connection successful"))
+    .catch((error) => console.log(error));
+
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.get('/episode', (req, res) => {
+    if(req.query.title) {
+        //business title search
+        BusinessModel.find( { title: new RegExp(req.query.title, "i") }, '_id deal description episode category entrepreneurs location website askedFor exchangeForStake valuation season shark1 shark2 shark3 shark4 shark5 title coords image comment', function (error, results) {
+            if (error) return (error);
+            res.send(results);
+        });
+    } else if(req.query.coords) {
+        let latString = req.query.coords.split(" ").join("").split(",")[0];
+        let longString = req.query.coords.split(" ").join("").split(",")[1];
+		let lat = Number(latString);
+		let long = Number(longString);
+        console.log(lat + "_" + long);
+        //coords search (finds businesses within 25 miles of search
+        BusinessModel.find({
+            coords: {
+             $geoNear: {
+              $maxDistance: 40000, 
+              $geometry: {
+               type: "Point",
+               coordinates: [long, lat],
+			   index: '2dsphere'  //probably unnecessary line but just in case
+              }
+             }
+            }
+           }).find((error, results) => {
+			    //console.log("Hello. Error happened here.");
+                console.log(error);
+                if (error) return (error);
+                res.send(results);
+           });
+    }
+});
+
+app.put('/episode', (req, res) => {
+    console.log('attempting to update entry with id: ' + req.query.id + ' with comment: ' + req.query.comment);
+    BusinessModel.updateOne({_id: req.query.id}, 
+        {comment: req.query.comment}, function (err, docs) {
+        if (err){
+            console.log(err)
+        }
+        else{
+            console.log("Updated Docs : ", docs);
+        }
+    });
+});
+
+app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
